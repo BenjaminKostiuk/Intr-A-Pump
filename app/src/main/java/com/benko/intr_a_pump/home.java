05b2d5a6f6c321b23c416690006c5e8b7e625a9d
Benjamin <benkostiuk1@gmail.com>
2019-01-26 17:27:14 -0500

    Added home page

--- app/src/main/java/com/benko/intr_a_pump/home.java
+++ app/src/main/java/com/benko/intr_a_pump/home.java
@@ -1,7 +1,9 @@
 package com.benko.intr_a_pump;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class home extends AppCompatActivity {
 
@@ -10,4 +12,25 @@ public class home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+    public void onPatientClick(View view) {
+        Intent intent = new Intent(this, PatientView.class);
+        startActivity(intent);
+    }
+
+    public void onAlertClick(View view) {
+        Intent intent = new Intent(this, AlertView.class);
+        startActivity(intent);
+    }
+
+    public void onScheduleClick(View view) {
+        Intent intent = new Intent(this, ScheduleView.class);
+        startActivity(intent);
+    }
+
+    public void onStatClick(View view) {
+        Intent intent = new Intent(this, StatsView.class);
+        startActivity(intent);
+    }
+
 }
