0dd5b6d40f13f2edee27271a968209f0a462e980
Benjamin <benkostiuk1@gmail.com>
2019-01-26 14:51:55 -0500

    Added splash page

--- app/src/main/java/com/benko/intr_a_pump/SplashPage.java
+++ app/src/main/java/com/benko/intr_a_pump/SplashPage.java
@@ -1,7 +1,9 @@
 package com.benko.intr_a_pump;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashPage extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SplashPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_page);
+    }
+
+    public void onSplashPageClick(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+        finish();
     }
 }
